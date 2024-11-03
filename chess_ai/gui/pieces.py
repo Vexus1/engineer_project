cb2d1042ca126239b20037eebb7a879b35035c9b
vexus1 <adriangalik123@gmail.com>
2024-11-03 23:58:13 +0100

    pieces class

---
+++ chess_ai/gui/pieces.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+
+import chess
+from chess import PieceType, Color
+from kivy.uix.image import Image
+
+IMAGES_DIR = 'gui/images'
+
+@dataclass
+class Piece:
+    type: PieceType
+    color: Color
+
+    def __post_init__(self):
+        pass
+
+    def load_pieces(self) -> list[tuple[Image, Image]]:
+        white_pawn = Image(source=f'{IMAGES_DIR}/white_pawn.png')
+        white_bishop = Image(source=f'{IMAGES_DIR}/white_bishop.png')
+        white_knight = Image(source=f'{IMAGES_DIR}/white_knight.png')
+        white_rook = Image(source=f'{IMAGES_DIR}/white_rook.png')
+        white_queen = Image(source=f'{IMAGES_DIR}/white_queen.png')
+        white_king = Image(source=f'{IMAGES_DIR}/white_king.png')
+        black_pawn = Image(source=f'{IMAGES_DIR}/black_pawn.png')
+        black_bishop = Image(source=f'{IMAGES_DIR}/black_bishop.png')
+        black_knight = Image(source=f'{IMAGES_DIR}/black_knight.png')
+        black_rook = Image(source=f'{IMAGES_DIR}/black_rook.png')
+        black_queen = Image(source=f'{IMAGES_DIR}/black_queen.png')
+        black_king = Image(source=f'{IMAGES_DIR}/black_king.png')
+        pieces = [(white_pawn, black_pawn),
+                  (white_bishop, black_bishop),
+                  (white_knight, black_knight),
+                  (white_rook, black_rook),
+                  (white_queen, black_queen),
+                  (white_king, black_king)]
+        return pieces
+    
+    def set_position(self, position) -> None:
+        self.position = position
+
+    def display_pieces(self):
+        pass
+
+        
